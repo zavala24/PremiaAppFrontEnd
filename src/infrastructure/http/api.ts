e5936b6f16c29608d49ba8e04e6e7e9b8fd4b3ba
Julio Cesar Zavala Cota <ing.juliozavala@gmail.com>
2025-11-19 11:20:40 -0600

    Cambios para hacer release en qa

--- src/infrastructure/http/api.ts
+++ src/infrastructure/http/api.ts
@@ -1,7 +1,8 @@
 // src/http/api.ts
 import axios, { AxiosError } from "axios";
 
-const BASE_URL = "http://192.168.1.9:5137/api";
+const BASE_URL =
+  "https://pymefiel-back-qa-f8g8dgeef7cjg5gd.canadacentral-01.azurewebsites.net/api";
 
 const create = () => {
   const i = axios.create({
